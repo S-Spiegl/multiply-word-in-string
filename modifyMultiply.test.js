befee5c2ad0d9ebff419cb34126e5ebe81fb5808
S-Spiegl <steven.spiegl@gmail.com>
2022-09-18 11:23:40 +0100

    all tests passing

--- modifyMultiply.test.js
+++ modifyMultiply.test.js
@@ -16,10 +16,22 @@ describe('modifyMultiply', () => {
   it('works for an even longer input with repeated word at later index pt2', () => {
     expect(modifyMultiply("This is a string",3,1)).toEqual("string")
   })
-  it('works joins output with hyphen', () => {
+  it('works joins output with hyphen 1', () => {
     expect(modifyMultiply("This is a string",3,2)).toEqual("string-string")
   })
-
+  it('works joins output with hyphen 2', () => {
+    expect(modifyMultiply("This is a string",3 ,5)).toEqual("string-string-string-string-string")
+  })
+  it('works joins output with hyphen 3', () => {
+    expect(modifyMultiply("Creativity is the process of having original ideas that have value. It is a process; it's not random.",8 ,10)).toEqual("that-that-that-that-that-that-that-that-that-that")
+  })
+  it('works joins output with hyphen 4', () => {
+    expect(modifyMultiply("Self-control means wanting to be effective at some random point in the infinite radiations of my spiritual existence",1 ,1)).toEqual("means")
+  })
+  it('works joins output with hyphen 4=5', () => {
+    expect(modifyMultiply("Is sloppiness in code caused by ignorance or apathy? I don't know and I don't care.",6 ,8)).toEqual("ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance")
+  })
+ 
 })
 
   // Test.assertEquals(modifyMultiply("This is a string",3 ,5), "string-string-string-string-string", "The string is incorrect");
